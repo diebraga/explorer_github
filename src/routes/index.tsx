64f74f9c9820c1c98425789f095a5910524f1b05
unknown <diebraga0055@gmail.com>
2020-05-16 16:36:21 +0100

    added routes pages

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../dashboard';
+import Repository from '../repository';
+
+export const Routes: React.FC = () => (
+  <Switch>
+  <Route path="/" exact component={Dashboard} />
+  <Route path="/repository" component={Repository} />
+
+</Switch>
+
+);
+
+export default Routes;
