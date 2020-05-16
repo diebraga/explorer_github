1fbce29368c39c007d44620cd881699debd26e62
unknown <diebraga0055@gmail.com>
2020-05-16 17:30:51 +0100

    added globals

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyles from './styles/globals';
 
-const App: React.FC = () => <BrowserRouter><Routes /></BrowserRouter>;
+const App: React.FC = () => <>
+<BrowserRouter>
+<Routes />
+</BrowserRouter>
+<GlobalStyles />
+</>;
+
 
 export default App;
---
+++ src/styles/globals.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+import GitBackground from '../assets/logo.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #F0F0F5 url(${GitBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+/**
+ *  Export globals to your App.tsx
+ */
