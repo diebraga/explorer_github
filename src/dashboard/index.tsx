638e4e70418a9e5bc049aa0d0bd83036cdea6b22
unknown <diebraga0055@gmail.com>
2020-05-16 21:42:26 +0100

    added form and title

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
-}
+import logoImg from '../assets/logosmall.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+    <>
+    <img src={logoImg} alt="git explorer" />
+    <Title>Explore Github Repositories</Title>
+
+    <Form>
+      <input placeholder="Search repository" />
+      <button type="submit">Go</button>
+    </Form>
+    </>
+);
 
 export default Dashboard;
