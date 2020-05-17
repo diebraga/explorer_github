2b3f75f6e6442221c558bf52705e6cdbf8266e73
unknown <diebraga0055@gmail.com>
2020-05-17 15:14:33 +0100

    added repositories styles

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../assets/logosmall.svg';
 
@@ -11,8 +12,36 @@ const Dashboard: React.FC = () => (
 
     <Form>
       <input placeholder="Search repository" />
-      <button type="submit">Go</button>
+      <button type="submit">Submit</button>
     </Form>
+
+    <Repositories>
+      <a href="">
+        <img
+          src="https://avatars2.githubusercontent.com/u/52054459?s=460&u=d4c512846e9d96d98c2da4eeb1c9906691461b80&v=4"
+          alt="Diego Braga"
+        />
+        <div>
+          <strong>aaaaaaaaaaaaa</strong>
+          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+      </a>
+      <a href="">
+        <img
+          src="https://avatars2.githubusercontent.com/u/52054459?s=460&u=d4c512846e9d96d98c2da4eeb1c9906691461b80&v=4"
+          alt="Diego Braga"
+        />
+        <div>
+          <strong>aaaaaaaaaaaaa</strong>
+          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+      </a>
+
+    </Repositories>
     </>
 );
 
--- src/dashboard/styles.ts
+++ src/dashboard/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Title = styled.h1`
-  font-size: 48px;
+  font-size: 45px;
   color: #3a3a3a;
   max-width: 450px;
   line-height: 56px;
@@ -48,5 +48,53 @@ export const Form = styled.form`
 `;
 
 export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
 
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    align-items: center;
+    display: flex;
+    transition: transform 2.0s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    svg {
+        margin-left: auto;
+        color: #A8A8B3;
+      }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p {
+        font-size: 18px;
+        margin-top: 4px;
+        color: #A8A8B3;
+      }
+    }
+  }
 `;
