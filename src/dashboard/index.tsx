c6f605d53ee9754146685aa633d9285850fb50c6
unknown <diebraga0055@gmail.com>
2020-05-18 18:27:25 +0100

    api connected

--- src/dashboard/index.tsx
+++ src/dashboard/index.tsx
@@ -1,48 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../services/api';
 
 import logoImg from '../assets/logosmall.svg';
 
 import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => (
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repos, setRepos] = useState<Repository[]>([]);
+
+  async function handleRepos(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepos([...repos, repository]);
+    setNewRepo('');
+  }
+  return (
     <>
     <img src={logoImg} alt="git explorer" />
     <Title>Explore Github Repositories</Title>
 
-    <Form>
-      <input placeholder="Search repository" />
+    <Form onSubmit={handleRepos}>
+      <input
+      value={newRepo}
+      onChange={(e) => setNewRepo(e.target.value)}
+      placeholder="Search repository" />
       <button type="submit">Submit</button>
     </Form>
 
     <Repositories>
-      <a href="">
-        <img
-          src="https://avatars2.githubusercontent.com/u/52054459?s=460&u=d4c512846e9d96d98c2da4eeb1c9906691461b80&v=4"
-          alt="Diego Braga"
-        />
-        <div>
-          <strong>aaaaaaaaaaaaa</strong>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
+      {repos.map((repository) => (
+              <a key={repository.full_name} href="repos">
+              <img
+                src={repository.owner.avatar_url}
+                alt={repository.owner.login}
+              />
+              <div>
+      <strong>{repository.full_name}</strong>
+      <p>{repository.description}</p>
+              </div>
 
-        <FiChevronRight size={20}/>
-      </a>
-      <a href="">
-        <img
-          src="https://avatars2.githubusercontent.com/u/52054459?s=460&u=d4c512846e9d96d98c2da4eeb1c9906691461b80&v=4"
-          alt="Diego Braga"
-        />
-        <div>
-          <strong>aaaaaaaaaaaaa</strong>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
+              <FiChevronRight size={20}/>
+            </a>
 
-        <FiChevronRight size={20}/>
-      </a>
-
+      ))}
     </Repositories>
     </>
-);
+  );
+};
 
 export default Dashboard;
