96ed9f2c8baac4cf9afda56eae0d4a2cdf29cd6f
unknown <diebraga0055@gmail.com>
2020-05-20 19:53:20 +0100

    dashboard styles added

--- src/repository/index.tsx
+++ src/repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { Header, RepoInfo, Issues } from './styles';
+
+import logoImg from '../assets/logosmall.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,58 @@ interface RepositoryParams {
 export const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+  <>
+  <Header>
+    <img src={logoImg} />
+
+    <Link to="//">
+    <FiChevronLeft size={20}/>
+
+      Back
+    </Link>
+  </Header>
+
+  <RepoInfo>
+    <header>
+      <img src="https://avatars2.githubusercontent.com/u/52054459?s=460&u=d4c512846e9d96d98c2da4eeb1c9906691461b80&v=4" alt="die" />
+      <div>
+        <strong>aaaaa</strong>
+        <p>aaaa</p>
+      </div>
+    </header>
+    <ul>
+      <li>
+        <strong> 1000</strong>
+        <span>stars</span>
+      </li>
+      <li>
+        <strong> 1000</strong>
+        <span>forks</span>
+      </li>
+      <li>
+        <strong> 1000</strong>
+        <span>issues</span>
+      </li>
+
+    </ul>
+  </RepoInfo>
+
+  <Issues>
+  <Link to="dddddfdf">
+              <div>
+      <strong>aaaaaaaaaaaaaaaaaa</strong>
+      <p>aaaaa</p>
+              </div>
+
+              <FiChevronRight size={20}/>
+            </Link>
+
+
+  </Issues>
+  </>
+  );
 };
+
 
 export default Repository;
